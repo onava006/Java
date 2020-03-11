da1c270e1b7422cbe43430d20fce952546ff000d
Oscar Navarrete <oscar.nvergara@gmail.com>
2020-03-11 10:11:54 -0300

    primer programa Java

---
+++ MyName/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi nombre es Coding Dojo");
+		System.out.println("Tengo 100 años de edad.");
+		System.out.println("Mi ciudad es Burbank, CA");		
+	}
+
+}
