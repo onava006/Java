2bad422dd483da0dde7ada276327c9ba594dd2be
Oscar Navarrete <oscar.nvergara@gmail.com>
2020-03-11 12:40:49 -0300

    FizzBuzz

---
+++ FizzBuzz/src/check/Main.java
@@ -0,0 +1,20 @@
+package check;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Fizz fiz = new Fizz();
+		System.out.println(fiz.fizzBuzz(3));
+
+		System.out.println(fiz.fizzBuzz(5));
+
+		System.out.println(fiz.fizzBuzz(15));
+
+		System.out.println(fiz.fizzBuzz(2));
+		
+	}
+
+}
