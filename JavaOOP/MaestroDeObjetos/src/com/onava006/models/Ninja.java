6b965481388f3d37f3fb8759f0a44d89942a78c2
Oscar Navarrete <oscar.n@outlook.com>
2020-06-17 17:22:07 -0400

    tarefas 2

--- JavaOOP/MaestroDeObjetos/src/com/onava006/models/Ninja.java
+++ JavaOOP/MaestroDeObjetos/src/com/onava006/models/Ninja.java
@@ -14,10 +14,30 @@ public class Ninja extends Human{
 	}
 	
 	public void steal(Human h) {
-		System.out.println("");
+		System.out.println("");	
+
+		Integer tosteal = this.getStealth();
+
+		if(tosteal + this.getHealth() >= this.getmaxHealth())
+		{
+			this.setHealth(this.getmaxHealth());
+			h.setHealth(this.getHealth() - tosteal);
+		} else {
+			this.setHealth(tosteal + this.getHealth());	
+			h.setHealth(this.getHealth() - tosteal);
+		}		
+		System.out.println(" el ninja  robó salud, su nueva salud es: " + this.getHealth());
 	}
 	
 	public void runAway() {
-		System.out.println("");
+		Integer escape = 10;
+		
+		if(this.getHealth() - escape > 0)
+		{
+			this.setHealth(this.getHealth() - escape);
+			System.out.println(" el ninja escapó, se le restaron 10 puntos de vida");
+		} else {
+			System.out.println(" el ninja es tan gil que intentando escapar se murió");
+		}	
 	}
 }
