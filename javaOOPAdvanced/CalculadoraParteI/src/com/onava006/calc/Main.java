46c9d21bcb89ac7cb332555c4790a38a07a404a0
Oscar Navarrete <oscar.n@outlook.com>
2020-06-18 11:50:17 -0400

    calculadora 1

---
+++ javaOOPAdvanced/CalculadoraParteI/src/com/onava006/calc/Main.java
@@ -0,0 +1,14 @@
+package com.onava006.calc;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Double uno = 2.4;
+		Double dos = 6.5;
+		Calculadora calculator = new Calculadora();
+		calculator.performOperation(uno, dos, "+");
+	}
+
+}
